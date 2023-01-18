14a0a6a8333d66e87c1e20c039ff371ba098736a
EdsonJSF <71522981+EdsonJSF@users.noreply.github.com>
2023-01-17 21:14:04 -0400

    configuracion del identado

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
