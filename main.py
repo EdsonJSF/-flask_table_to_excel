fc9003ce05ca03edae165048777957e6273dfdea
EdsonJSF <edsonjsf1990@gmail.com>
2023-01-20 19:42:33 -0400

    Se agrego xlwt para generar descargas de archivos excel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import io
+import xlwt
 import os
 
 app = Flask(__name__)
@@ -6,6 +8,34 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/table/download')
+def download_report():
+    result = [
+        ["id", "data_1", "data_2"],
+        ["id", "data_1", "data_2"],
+        ["id", "data_1", "data_2"],
+        ["id", "data_1", "data_2"],
+    ]
+    output = io.BytesIO()
+    wb = xlwt.Workbook()
+    ws = wb.add_sheet('contenido')
+
+    ws.write(0,0,"Title_1")
+    ws.write(0,1,"Title_2")
+    ws.write(0,2,"Title_3")
+
+    idx = 0
+    for row in result:
+        ws.write(idx+1, 0, f"row {idx+1}")
+        ws.write(idx+1, 1, f"row {idx+1}")
+        ws.write(idx+1, 2, f"row {idx+1}")
+        idx += 1
+
+    wb.save(output)
+    output.seek(0)
+
+    return Response(output, mimetype="application/ms-excel", headers={"Content-Disposition":"attachment;filename=data.xls"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
